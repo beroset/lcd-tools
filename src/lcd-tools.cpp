5fc8dae24cdb4521b5529b5c644dd94bc789e08e
Ed Beroset <beroset@ieee.org>
2026-07-28 19:20:54 -0400

    Simplify lcd-tools
    This simplifies lcd-tools by using a unified Feature list to assure
    consistent feature flag spelling and description and removes much
    duplicated code by using a data-driven design.  This makes it easier to
    consistently name features, to add them, and to add new watches.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/lcd-tools.cpp
+++ src/lcd-tools.cpp
@@ -3,62 +3,35 @@
 #include "koi-tools.h"
 #include "medaka-tools.h"
 
-#include <CLI/CLI.hpp>
-
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
 #include <QSettings>
 
 const char* CONFIG_FILE = "/etc/asteroid/machine.conf";
+CLI::App IWatch::app{"lcd-tools: Various controls for watches with a secondary display"};
 
 int main( int argc, char** argv ) {
-
 	QSettings m_settings(CONFIG_FILE, QSettings::IniFormat);
-	const QString machineCodename = m_settings.value("Identity/MACHINE", "unknown").toString();
+	const std::string machineCodename = m_settings.value("Identity/MACHINE", "unknown").toString().toStdString();
 
-	CLI::App app{"lcd-tools: Various controls for watches with a secondary display"};
-	if (machineCodename == "koi") {
-		using namespace AsteroidOS::LCD_Tools::Koi;
-		app.add_flag("--sync-time",SyncTime,"Sync lcd time with linux time");
-		app.add_flag("--white-background",[](int){SetDisplayColor(true);},"(Koi) set display background to white");
-		app.add_flag("--black-background",[](int){SetDisplayColor(false);},"(Koi) set display background to black");
-		app.add_flag("--prepare-timepiece",PrepareTimepiece,"(Koi) prepare watch for power off into timekeeping mode. You will then need to shut it down manually");
-		app.add_flag("--session-restart",SyncSettings,"Initialise LCD for user session restart");
-		CLI11_PARSE(app, argc, argv);
-	}
-	if (machineCodename == "medaka") {
-		using namespace AsteroidOS::LCD_Tools::Medaka;
-		app.add_flag("--sync-time",SyncTime,"Sync lcd time with linux time");
-		app.add_flag("--prepare-timepiece",PrepareTimepiece,"(Medaka) prepare watch for power off into timekeeping mode. You will then need to shut it down manually");
-		app.add_flag("--white-background",[](int){SetDisplayColor(true);},"(Medaka) set display background to white");
-		app.add_flag("--black-background",[](int){SetDisplayColor(false);},"(Medaka) set display background to black");
-		app.add_flag("--session-restart",SyncSettings,"Initialise LCD for user session restart");
-		CLI11_PARSE(app, argc, argv);
-	}
-	else if (machineCodename == "catfish") {
-		CatfishLcdTools catfish;
-		app.add_flag("--sync-time",[&catfish](int){ catfish.SyncTime(); },"Sync lcd time with linux time");
-		app.add_flag("--prepare-timepiece",[&catfish](int){ catfish.PrepareTimepiece(); },"(Catfish) Start timepiece only mode");
-		app.add_flag("--enable-stepcounter",[&catfish](int){ catfish.EnableStepCounter(); },"(Catfish) Enable the step counter");
-		app.add_flag("--disable-stepcounter",[&catfish](int){ catfish.DisableStepCounter(); },"(Catfish) Disable the step counter");
-		app.add_flag("--enable-heartrate",[&catfish](int){ catfish.EnableHeartRate(); },"(Catfish) Enable the heart rate sensor");
-		app.add_flag("--disable-heartrate",[&catfish](int){ catfish.DisableHeartRate(); },"(Catfish) Disable the heart rate sensor");
-		app.add_flag("--enable-motion",[&catfish](int){ catfish.EnableMotion(); },"(Catfish) Enable motion");
-		app.add_flag("--disable-motion",[&catfish](int){ catfish.DisableMotion(); },"(Catfish) Disable motion");
-		app.add_flag("--session-restart","Initialise LCD for user session restart"); // this isn't used on catfish, but the service to error out if this option isn't present.
-		CLI11_PARSE(app, argc, argv);
-	}
-	else if (machineCodename ==  "rubyfish") {
-		RubyfishLcdTools rubyfish;
-		app.add_flag("--sync-time",[&rubyfish](int){ rubyfish.SyncTime(); },"Sync lcd time with linux time");
-		app.add_flag("--prepare-timepiece",[&rubyfish](int){ rubyfish.PrepareTimepiece(); },"(rubyfish) Start timepiece only mode");
-		app.add_flag("--enable-stepcounter",[&rubyfish](int){ rubyfish.EnableStepCounter(); },"(rubyfish) Enable the step counter");
-		app.add_flag("--disable-stepcounter",[&rubyfish](int){ rubyfish.DisableStepCounter(); },"(rubyfish) Disable the step counter");
-		app.add_flag("--enable-heartrate",[&rubyfish](int){ rubyfish.EnableHeartRate(); },"(rubyfish) Enable the heart rate sensor");
-		app.add_flag("--disable-heartrate",[&rubyfish](int){ rubyfish.DisableHeartRate(); },"(rubyfish) Disable the heart rate sensor");
-		app.add_flag("--enable-motion",[&rubyfish](int){ rubyfish.EnableMotion(); },"(rubyfish) Enable motion");
-		app.add_flag("--disable-motion",[&rubyfish](int){ rubyfish.DisableMotion(); },"(rubyfish) Disable motion");
-		app.add_flag("--session-restart","Initialise LCD for user session restart"); // this isn't used on rubyfish, but the service to error out if this option isn't present.
-		CLI11_PARSE(app, argc, argv);
-	}
+	using Creator = std::function<std::unique_ptr<IWatch>()>;
+	using Watchlist = std::unordered_map<std::string_view, Creator>;
 
-	return 0;
+	static Watchlist watches = {
+		{ "catfish",  []{ return std::make_unique<Catfish>(); } },
+		{ "rubyfish", []{ return std::make_unique<Rubyfish>(); } },
+		{ "koi",      []{ return std::make_unique<Koi>(); } },
+		{ "medaka",   []{ return std::make_unique<Medaka>(); } },
+	};
+	try {
+		auto a = watches.at(machineCodename)();
+		a->run(argc, argv);
+	} catch (std::out_of_range&) {
+		// not in the list of watches, so do nothing
+	}
 }
